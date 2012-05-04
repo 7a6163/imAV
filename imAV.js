419f0930e170b2301e1b89ee80c2a33bbad3c28b
Zac <mymengyu@gmail.com>
2012-05-04 22:34:40 +0800

    Initial Version

--- imAV.js
+++ imAV.js
@@ -1,7 +1,35 @@
 // ==UserScript==
 // @name			imAV
-// @namespace		http://www.webmonkey.com
-// @description		
+// @namespace		imAV
+// @description		Easy
 // @include			http://vod.im.tv/*
-// @version			0.1
+// @auther			Zac
+// @version			0.1.2
 // ==/UserScript==
+
+
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('(' + main + ')();'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
+function main() {
+	'use strict';
+	var queryParameter = location.search,
+		patten = /\d{4,5}/gi,
+		vodid = patten.exec(queryParameter)[0],
+		divContent = document.getElementById('content_photo'),
+		imgContent = divContent.firstChild,
+		lnkVideo = document.createElement('a'),
+		url = getUrl(vodid);
+		
+		lnkVideo.setAttribute('href',url);
+		divContent.appendChild(lnkVideo).appendChild(imgContent);
+		
+		function getUrl(vodid) {
+			if (vodid === '') {
+				return false;
+			}
+			var url = 'http://v1.im.tv/PaidVOD/vod/' + vodid.slice(0,1) + '/' + vodid.slice(1,2) + '/' + vodid + '.wmv';
+			return url;	
+		}
+}
